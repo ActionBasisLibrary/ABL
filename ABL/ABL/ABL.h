c9a6b96cac8825ada12481c5792edc048958132e
Matt Rubin <mattrubin@me.com>
2012-04-24 20:52:49 -0700

    Added empty project with a static library target
    Based on the Cocoa Touch Static Library template.

---
+++ ABL/ABL/ABL.h
@@ -0,0 +1,13 @@
+//
+//  ABL.h
+//  ABL
+//
+//  Created by Matt Rubin on 4/24/12.
+//  Copyright (c) 2012 Matt Rubin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABL : NSObject
+
+@end
---
+++ ABL/ABL/ABL.m
@@ -0,0 +1,13 @@
+//
+//  ABL.m
+//  ABL
+//
+//  Created by Matt Rubin on 4/24/12.
+//  Copyright (c) 2012 Matt Rubin. All rights reserved.
+//
+
+#import "ABL.h"
+
+@implementation ABL
+
+@end
