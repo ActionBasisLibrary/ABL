b0768da0da412e5af648f5c55008e9e36eb74a12
Matt Rubin <mattrubin@me.com>
2012-04-24 21:03:08 -0700

    Added "ABL on iOS" demo app

---
+++ Examples/ABL on iOS/ABL on iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ABL on iOS
+//
+//  Created by Matt Rubin on 4/24/12.
+//  Copyright (c) 2012 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Examples/ABL on iOS/ABL on iOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ABL on iOS
+//
+//  Created by Matt Rubin on 4/24/12.
+//  Copyright (c) 2012 Matt Rubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
