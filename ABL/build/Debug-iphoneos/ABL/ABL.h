18c368803ec49beccabc7d2849d8feb361a06493
Caleb Jordan <greenstrand@gmail.com>
2012-05-03 14:29:26 -0700

    Messed with build settings to try to make it build

---
+++ ABL/build/Debug-iphoneos/ABL/ABL.h
@@ -0,0 +1,17 @@
+//
+//  ABL.h
+//  ABL
+//
+//  Created by Matt Rubin on 4/24/12.
+//  Copyright (c) 2012 Matt Rubin. All rights reserved.
+//
+
+#ifndef ABL_h
+#define ABL_h
+
+class ABL {
+public:
+	static void test();
+};
+
+#endif
